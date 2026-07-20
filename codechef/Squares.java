c090e158499be64ae780b610f0899371a8be87a6
Madhumitha <rmadhumitha675@gmail.com>
2026-07-20 19:30:41 +0530

    Create Squares.java

---
+++ codechef/Squares.java
@@ -0,0 +1,13 @@
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+         int num = 1;
+        while (num <= 5)
+        {
+           System.out.print(num * num + " ");
+           num++;
+        }
+	}
+}
